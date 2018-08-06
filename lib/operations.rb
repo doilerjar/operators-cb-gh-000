7182c960fdb354b40f4f0b8435336ff063f4cfd7
Kevin K <doilerjar@outlook.com>
2018-08-06 22:31:03 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,11 @@
 def unsafe?(speed)
-  not_safe = false
-  speed > 60 ? return true : false
   speed < 40 ? true : false
 end
 
 
 
 def not_safe?(speed)
-
+  speed > 60 ? return true : (speed < 40 ? return true : (return false))
 end
 	
 
